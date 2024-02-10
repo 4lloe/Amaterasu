cd4ea66a57c971fb8954f3d23f1089b36eba51bf
4lloe <m4lyid4nil4@gmail.com>
2024-02-10 23:02:02 +0200

    Repo creation

---
+++ src/main/java/com/example/Amaterasu/AmaterasuApplication.java
@@ -0,0 +1,13 @@
+package com.example.Amaterasu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmaterasuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmaterasuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Amaterasu/AmaterasuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Amaterasu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmaterasuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
